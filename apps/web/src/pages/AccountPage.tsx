57fc5b9b25f021e52c980cc939db6bbc7d6c59a9
Alexandre Lavoie <36684879+alexandre-lavoie@users.noreply.github.com>
2024-01-08 10:39:33 -0500

    Auth UI with Backend (#7)

---
+++ apps/web/src/pages/AccountPage.tsx
@@ -0,0 +1,28 @@
+import { useAuth } from "@repo/ui/auth";
+import { Header } from "@repo/ui/header";
+import { Avatar, AvatarFallback, AvatarImage } from "@repo/shadcn/components/ui/avatar"
+
+export interface ProjectStatusBadgeProps {
+  status: "complete" | "crack" | "open";
+}
+
+export const AccountPage = () => {
+  const { username } = useAuth();
+
+  return (
+    <div>
+      <Header />
+      <div className="grid justify-center p-4">
+        <div className="grid justify-center gap-2">
+          <div className="grid justify-center">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <h1 className="text-xl font-semibold">{username}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
